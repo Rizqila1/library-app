de5e74520569545a9e664dd02bd2554458110c4b
Rizqila1 <ikilatr@gmail.com>
2024-01-17 21:13:22 +0700

    feat: fix identity name

--- src/controllers/c_books.js
+++ src/controllers/c_books.js
@@ -157,7 +157,7 @@ const updateBook = async (req, res) => {
           payload = {
             ...body,
             ...payload,
-            book_name: body.name?.trim(),
+            book_name: body.book_name?.trim(),
           };
 
           const newData = await ModelBooks.findByIdAndUpdate(
